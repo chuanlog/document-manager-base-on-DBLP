f86dba5f9b23f75960faaf7c9c217c368d5947f4
cloog <395773508@qq.com>
2025-03-10 18:14:03 +0800

    创建SpringBoot项目脚手架

---
+++ src/main/java/com/scut/documentmanagerbaseondbpl/DocumentManagerBaseOnDbplApplication.java
@@ -0,0 +1,13 @@
+package com.scut.documentmanagerbaseondbpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerBaseOnDbplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentManagerBaseOnDbplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scut/documentmanagerbaseondbpl/DocumentManagerBaseOnDbplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scut.documentmanagerbaseondbpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerBaseOnDbplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
